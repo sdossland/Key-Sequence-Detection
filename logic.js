e982d74a526f62756e3671402416967eefe0c48a
Sophia Dossland <sophia.dossland@gmail.com>
2017-03-17 15:24:48 -0700

    add comments to code, fix css positioning of 'hints'

--- logic.js
+++ logic.js
@@ -8,16 +8,19 @@ document.addEventListener("DOMContentLoaded", function () {
   let unicornCount = 0;
 
   window.addEventListener('keydown', function(e) {
+    //push each new character to the back of the array
     attempts.push(e.key);
+    //cut the last 6 letters from the array, then join them together into a word
     let submission = attempts.slice(-6).join('');
+    //log submission to the screen for the user to see
     document.querySelector('.output').innerHTML = submission;
+    //check if last six keys hit equal the secret word
     if (submission.toLowerCase().includes(secretWord.toLowerCase())) {
-      console.log(unicornCount);
       if (unicornCount === 0) {
         document.querySelector('h3').style.display = 'none';
         document.querySelector('ol').style.display = 'none';
       }
-      document.querySelector('h1').innerHTML = 'Keep typing the six letter word to collect more unicorns!';
+      document.querySelector('h1').innerHTML = 'Keep typing the six letter <br/> word to collect more unicorns!';
       cornify_add();
       unicornCount++;
     }
